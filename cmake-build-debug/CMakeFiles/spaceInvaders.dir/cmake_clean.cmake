8c194ac47428e0184be81bdc85d7027fd23b62e9
Zupanico <50524719+Zupanico@users.noreply.github.com>
2021-05-01 14:00:53 -0400

    Création du projet Space Invaders

---
+++ cmake-build-debug/CMakeFiles/spaceInvaders.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/spaceInvaders.dir/main.cpp.obj"
+  "libspaceInvaders.dll.a"
+  "spaceInvaders.exe"
+  "spaceInvaders.exe.manifest"
+  "spaceInvaders.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/spaceInvaders.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
